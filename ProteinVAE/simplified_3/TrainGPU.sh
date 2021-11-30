e5deeaccf9e565f3aa230509cb102026833f26ef
Archie Yao <461731182@qq.com>
2021-11-30 11:37:52 -0500

    add simplified 3 (actually not simplified lol)

---
+++ ProteinVAE/simplified_3/TrainGPU.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+#SBATCH -J Sim3ProVAE # Job name 
+#SBATCH -n 16 # Number of total cores 
+#SBATCH -N 1 # Number of nodes 
+#SBATCH -A venkvis_gpu
+#SBATCH --gres=gpu:1
+#SBATCH -p gpu 
+#SBATCH --mem-per-cpu=2000 # Memory pool for all cores in MB 
+#SBATCH --time=14-00:00:00
+#SBATCH -e error_%j.err 
+#SBATCH -o out_%j.out # File to which STDOUT will be written %j is the job # 
+#SBATCH --mail-type=ALL # Type of email notification- BEGIN,END,FAIL,ALL 
+#SBATCH --mail-user=mingzeya@andrew.cmu.edu # Email to which notifications will be sent 
+
+echo "Job started on `hostname` at `date`" 
+CRTDIR=$(pwd) # get current work directory
+echo $CRTDIR
+cd $CRTDIR
+
+source /home/mingzeya/miniconda3.sh 
+
+conda activate abnn
+
+module load cuda/10.2.89 
+
+cd $CRTDIR
+
+python3 train.py
+
+echo " " 
+ echo "Job Ended at `date`" 
